a164bae4bb6258b7adabdf0d1db362a86d8d5422
canil <luca.canizzo@gmail.com>
2022-02-21 14:45:21 -0500

    Compiles value errors.

--- src/compile_value_errors.py
+++ src/compile_value_errors.py
@@ -1,9 +1,34 @@
 # %%
 import pandas as pd
 from os import listdir, remove as os_remove
-from os.path import isfile, join
+from os.path import isfile, join, exists
 
 path = './etl_val_errors'
+compiled_path = f'{path}/compiled/errors.csv'
+summary_path = f'{path}/compiled/summary.csv'
+
+
+def pivot_errors(df):
+    """
+    Takes compiled errors dataframe and pivots error values under
+    column name columns.
+    """
+    return df.pivot_table(values='value', index=df.index, columns='column', aggfunc='first')
+
+
+def condense_summary(summary_df):
+    """
+    Reads the summary error csv and condenses to a more readable dataframe.
+    """
+    error_cols = []
+
+    for columnName in summary_df:
+        error_vals = summary_df[~summary_df[columnName].isnull()]
+        col_frame = pd.DataFrame(error_vals, columns=[columnName])
+        col_frame = col_frame.reset_index(drop=True)
+        error_cols.append(col_frame)
+
+    return pd.concat(error_cols, axis=1)
 
 
 if __name__ == '__main__':
@@ -15,13 +40,15 @@ if __name__ == '__main__':
         file_path = f'{path}/{file_name}'
         error_frames.append(pd.read_csv(file_path))
         os_remove(file_path)
-    
-    errors = pd.concat(error_frames)
-    # %%
-    errors.head()
-    print(len(errors.index))
 
-    errors.drop_duplicates(inplace=True)
-    print(len(errors.index))
+    if error_frames:
+        compiled_errors = pd.concat(error_frames)
+        compiled_errors.drop_duplicates(inplace=True)
+        compiled_errors = pivot_errors(compiled_errors)
+        compiled_errors.to_csv(summary_path)
 
-    errors.to_csv(f'{path}/errors.csv')
+    if exists(compiled_path):
+        compiled_errors = pivot_errors(pd.read_csv(compiled_path))
+        compiled_errors = condense_summary(compiled_errors)
+        print(compiled_errors.head())
+        compiled_errors.to_csv(summary_path)
